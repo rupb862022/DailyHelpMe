91c69600da6a419021b1e38012754f025bc777f9
yahelb <93283346+yaedgil@users.noreply.github.com>
2022-03-13 09:21:28 +0200

    ourvisualstudio

---
+++ FinalProject/WebApplication/Controllers/UsersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DATA;
+
+namespace WebApplication.Controllers
+{
+    public class UsersController : ApiController
+    {
+        DailyHelpMeDBContext db = new DailyHelpMeDBContext();
+
+        // GET: api/Users
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Users/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Users
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Users/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Users/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
